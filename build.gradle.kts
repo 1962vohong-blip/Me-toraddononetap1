e797dc75d521bfc6b2db357cec686fbfbb95f8be
1962vohong-blip <1962vohong@gmail.com>
2026-08-11 19:56:32 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("net.fabricmc.fabric-loom") version "1.14.3"
+    id("net.fabricmc.fabric-loom-remap") version "1.14.3"
 }
 
 version = "1.0.0"
@@ -8,6 +8,7 @@ group = "com.example.mod"
 repositories {
     mavenCentral()
     maven("https://maven.fabricmc.net/")
+    maven("https://maven.meteordev.org/")
 }
 
 dependencies {
@@ -15,7 +16,7 @@ dependencies {
     "mappings"("net.fabricmc:yarn:1.21.11+build.1:v2")
     
     "modImplementation"("net.fabricmc:fabric-loader:0.16.5")
-    "modImplementation"("net.fabricmc.fabric-api:fabric-api:0.108.0+1.21.11")
+    "modImplementation"("net.fabricmc:fabric-api:fabric-api:0.108.0+1.21.11")
 }
 
 java {
