d117747a8d276588482c5383b348c4de9033d96a
1962vohong-blip <1962vohong@gmail.com>
2026-08-11 20:21:18 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("net.fabricmc.fabric-loom") version "1.14.3"
+    id("net.fabricmc.fabric-loom") version "1.7-SNAPSHOT"
 }
 
 version = "1.0.0"
@@ -12,9 +12,7 @@ repositories {
 
 dependencies {
     "minecraft"("com.mojang:minecraft:1.21.11")
-    
-    // Sử dụng add() để tránh lỗi xung đột cấu hình trên Gradle mới
-    add("mappings", "net.fabricmc:yarn:1.21.11+build.1:v2")
+    "mappings"("net.fabricmc:yarn:1.21.11+build.1:v2")
     
     "modImplementation"("net.fabricmc:fabric-loader:0.16.5")
     "modImplementation"("net.fabricmc.fabric-api:fabric-api:0.108.0+1.21.11")
