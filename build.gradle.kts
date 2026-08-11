795ec3ed5f7493ab3458f532b354e9bdedc36185
1962vohong-blip <1962vohong@gmail.com>
2026-08-11 19:33:16 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("net.fabricmc.fabric-loom") version "1.14.3"
+    id("net.fabricmc.fabric-loom-remap") version "1.14.3"
 }
 
 version = "1.0.0"
@@ -11,18 +11,13 @@ repositories {
     maven("https://maven.meteordev.com/")
 }
 
-// Chủ động khởi tạo configuration để Gradle 9 nhận diện ngay từ đầu
-configurations {
-    maybeCreate("mappings")
-}
-
 dependencies {
-    "minecraft"("com.mojang:minecraft:1.21.11")
-    "mappings"("net.fabricmc:yarn:1.21.11+build.1:v2")
+    minecraft("com.mojang:minecraft:1.21.11")
+    mappings("net.fabricmc:yarn:1.21.11+build.1:v2")
     
-    "modImplementation"("net.fabricmc:fabric-loader:0.16.5")
-    "modImplementation"("net.fabricmc:fabric-api:fabric-api:0.108.0+1.21.11")
-    "modImplementation"("meteordevelopment:meteor-client:0.5.8")
+    modImplementation("net.fabricmc:fabric-loader:0.16.5")
+    modImplementation("net.fabricmc:fabric-api:fabric-api:0.108.0+1.21.11")
+    modImplementation("meteordevelopment:meteor-client:0.5.8")
 }
 
 java {
