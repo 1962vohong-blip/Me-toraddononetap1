f6fa0c5c3f1aa7baf411ccb85b64b7c0cc482e64
1962vohong-blip <1962vohong@gmail.com>
2026-08-11 21:23:59 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,11 +12,11 @@ repositories {
 }
 
 dependencies {
-    "minecraft"("com.mojang:minecraft:1.21.11")
-    "mappings"("net.fabricmc:yarn:1.21.11+build.1:v2")
+    "minecraft"("com.mojang:minecraft:1.21.4")
+    "mappings"("net.fabricmc:yarn:1.21.4+build.3:v2")
     
     "modImplementation"("net.fabricmc:fabric-loader:0.16.5")
-    "modImplementation"("net.fabricmc:fabric-api:fabric-api:0.108.0+1.21.11")
+    "modImplementation"("net.fabricmc.fabric-api:fabric-api:0.110.0+1.21.4")
 }
 
 java {
