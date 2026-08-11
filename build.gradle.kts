d1287e502a04d134281a67b55efcb536ef69cdcb
1962vohong-blip <1962vohong@gmail.com>
2026-08-11 18:50:24 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,13 +7,15 @@ group = "com.example.mod"
 
 repositories {
     mavenCentral()
-    maven("https://maven.meteordev.com/")
     maven("https://maven.fabricmc.net/")
+    maven("https://maven.meteordev.com/")
 }
 
 dependencies {
+    // Sử dụng cú pháp khai báo trực tiếp để bỏ qua lỗi libs.versions.toml
     minecraft("com.mojang:minecraft:1.21.11")
     mappings("net.fabricmc:yarn:1.21.11+build.1:v2")
+    
     modImplementation("net.fabricmc:fabric-loader:0.16.5")
     modImplementation("net.fabricmc.fabric-api:fabric-api:0.108.0+1.21.11")
     modImplementation("meteordevelopment:meteor-client:0.5.8")
@@ -23,9 +25,8 @@ java {
     toolchain.languageVersion.set(JavaLanguageVersion.of(21))
 }
 
-tasks.processResources {
-    inputs.property("version", project.version)
-    filesMatching("fabric.mod.json") {
-        expand(mapOf("version" to project.version))
+tasks.withType<org.gradle.jvm.tasks.Jar> {
+    from("LICENSE") {
+        rename { "${it}_${project.name}" }
     }
 }
