a8d95aaa8b42988870029491ef5815fe2e1866d7
1962vohong-blip <1962vohong@gmail.com>
2026-08-11 17:58:39 +0700

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,26 +2,6 @@ pluginManagement {
     repositories {
         maven("https://maven.fabricmc.net/")
         gradlePluginPortal()
-        google()
-        mavenCentral()
-    }
-}
-
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        maven("https://maven.fabricmc.net/")
-        maven("https://maven.meteordev.org/releases")
-        maven("https://maven.meteordev.org/snapshots")
-        mavenCentral()
-    }
-}
-
-rootProject.name = "Me-toraddononetap1"
-pluginManagement {
-    repositories {
-        maven("https://maven.fabricmc.net/")
-        gradlePluginPortal()
     }
 }
 
