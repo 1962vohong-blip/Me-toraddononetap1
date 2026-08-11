1a27bab319aad5c656ec628b8d86092b90e5e1f1
1962vohong-blip <1962vohong@gmail.com>
2026-08-11 20:02:28 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("net.fabricmc.fabric-loom-remap") version "1.14.3"
+    id("net.fabricmc.fabric-loom") version "1.14.3"
 }
 
 version = "1.0.0"
@@ -8,15 +8,20 @@ group = "com.example.mod"
 repositories {
     mavenCentral()
     maven("https://maven.fabricmc.net/")
-    maven("https://maven.meteordev.org/")
 }
 
 dependencies {
-    "minecraft"("com.mojang:minecraft:1.21.11")
-    "mappings"("net.fabricmc:yarn:1.21.11+build.1:v2")
+    // 1. Minecraft
+    minecraft("com.mojang:minecraft:1.21.11")
     
-    "modImplementation"("net.fabricmc:fabric-loader:0.16.5")
-    "modImplementation"("net.fabricmc:fabric-api:fabric-api:0.108.0+1.21.11")
+    // 2. Mappings (Chính xác cho 1.21.11)
+    mappings("net.fabricmc:yarn:1.21.11+build.1:v2")
+    
+    // 3. Fabric Loader
+    modImplementation("net.fabricmc:fabric-loader:0.16.5")
+    
+    // 4. Fabric API (Đã sửa đúng Group ID)
+    modImplementation("net.fabricmc.fabric-api:fabric-api:0.108.0+1.21.11")
 }
 
 java {
